b3768c3b190a9bc3a4100b9e069b26888af9056a
Suvo Ghosh <suvosh663@gmail.com>
2025-03-01 14:27:10 +0530

    update CORS configuration to allow requests from Netlify deployment

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://learnity123.netlify.app", credentials: true }));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
